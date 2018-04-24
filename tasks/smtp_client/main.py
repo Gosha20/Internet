00471ff33ecab59cccea747a0c82ff4b91e2ef87
Gosha20 <gosha3548@gmail.com>
2018-04-24 20:47:57 +0500

    update
    update

--- tasks/smtp_client/main.py
+++ tasks/smtp_client/main.py
@@ -1,5 +1,6 @@
 import os
 import base64
+import re
 import ssl
 import socket
 from configparser import ConfigParser
@@ -10,14 +11,15 @@ TYPES_CONTENTS = {'png': 'image/png', 'jpg': 'image/jpeg', 'txt': 'text/plain'}
 LOGIN = base64.b64encode(b'nabgosha@yandex.ru')
 PASSWORD = base64.b64encode(b'nabgosha123456')
 IP_PORT = ('smtp.yandex.ru', 465)
+PATH_TO_DIR = os.path.normpath(os.path.join(os.path.dirname(__file__), 'for_files'))
 
 class Mail:
-    def __init__(self, from_name, to_name, subj, attachment, letter):
+    def __init__(self, from_name, to_name, subj, attachment,letter):
         self.from_name = from_name
         self.to_names = to_name
         self.subject = subj
         self.attachments = attachment
-        self.letter = letter
+        self.letter = self.create_letter_in_byte(letter)
 
     def get_from(self):
         return bytes(self.from_name, 'utf-8')
@@ -28,29 +30,44 @@ class Mail:
     def get_subj(self):
         return b'=?utf-8?B?' + base64.b64encode(bytes(self.subject, 'utf-8')) + b'?='
 
-    def create_letter_in_byte(self):
-        letter = self.letter.encode()
-        return letter
+    def create_letter_in_byte(self, letter):
+        with open(os.path.join(PATH_TO_DIR, letter), 'r', encoding='utf-8') as f:
+            message = ''
+            line = f.readline()
+            while line:
+                if re.match('^\.+\n$', line):
+                    message += line[:-1] + '.\n'
+                elif re.match('^\.+$', line):
+                    message += line + '.'
+                else:
+                    message += line
+                line = f.readline()
+
+            letter = message.encode()
+            return letter
 
     def create_msg(self):
         result = b'From: ' + self.get_from() + b'\n'
         result += b'To: ' + self.get_to() + b'\n'
         result += b'Subject: ' + self.get_subj() + b'\n'
         result += b'MIME-Version: 1.0' + b'\n'
-        result += b'Content-Type: multipart/mixed; boundary="boundary"\n\n'
-        result += b'--boundary\n'
+        if len(self.attachments) != 0:
+            result += b'Content-Type: multipart/mixed; boundary="boundary"\n\n'
+            result += b'--boundary\n'
         result += b'Content-Type: text/plain;\n\n'
-        result += self.create_letter_in_byte() + b'\n'
-        result += b'--boundary\n'
-        result += self.attachments
-        result += b'--\n.'
+        result += self.letter + b'\n'
+        if len(self.attachments) != 0:
+            result += b'--boundary'
+            result += b'\n' + self.attachments
+            result += b'--'
+        result+=b'\n.'
         return result
 
 
 def settings():
     global PASSWORD
-    path_to_dir = os.path.normpath(os.path.join(os.path.dirname(__file__), 'for_files'))
-    path_to_config = os.path.join(path_to_dir, CONFIG_FILE)
+
+    path_to_config = os.path.join(PATH_TO_DIR, CONFIG_FILE)
     parser = ConfigParser()
     parser.read(path_to_config, encoding='utf-8')
 
@@ -63,16 +80,15 @@ def settings():
     for attachment in attachs.split('\n')[1:]:
         file = attachment.split(', ')[0]
         mime_type = attachment.split(', ')[1]
-        with open(os.path.join(path_to_dir, file), 'rb') as f:
+        with open(os.path.join(PATH_TO_DIR, file), 'rb') as f:
             file_b = base64.b64encode(f.read())
             attachments_ready_to_send += f'Content-Disposition: attachments; filename="{file}"'.encode() + b'\n'
             attachments_ready_to_send += b'Content-Transfer-Encoding: base64' + b'\n'
             attachments_ready_to_send += f'Content-Type: {mime_type}; name="{file}"'.encode()
             attachments_ready_to_send += b'\n\n' + file_b + b'\n'
             attachments_ready_to_send += b'--boundary'
-    with open(os.path.join(path_to_dir, parser.get('Letter', 'File_name')), 'r', encoding='utf-8') as f:
-        letter = f.read()
-    return Mail(from_name, to_names, subject, attachments_ready_to_send, letter)
+
+    return Mail(from_name, to_names, subject, attachments_ready_to_send, parser.get('Letter', 'File_name'))
 
 
 def send_recv(str, sock):
@@ -86,7 +102,7 @@ if __name__ == '__main__':
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock = ssl.wrap_socket(sock)
         sock.connect(IP_PORT)
-        sock.timeout(30)
+        sock.settimeout(30)
         try:
             print(sock.recv(1024))
             print(send_recv(b'EHLO friens', sock))
