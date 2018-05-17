94f8746743958962a711f7137291eb5d6654ffe2
Gosha20 <gosha3548@gmail.com>
2018-05-17 16:30:20 +0500

    END

--- tasks/pop3/pop3.py
+++ tasks/pop3/pop3.py
@@ -8,25 +8,27 @@ import sys
 BOUNDARY = "#-#-#-#--#-#-#-#--#-#-#-#--#-#-#-#--#-#-#-#--#-#-#-#--#-#-#--#-#-"
 regex = re.compile('(Subject: |\t)=\?utf-8\?B\?(.*?)\?=')
 
-def send_recv(command, sock):
-	command_to_send = bytes(command, 'utf-8') + b'\n'
 
-	sock.send(command_to_send)
-	data_to_send = b''
-	while True:
-		try:
-			data = sock.recv(1024)
-		except socket.timeout:
-			break
-		data_to_send += data
-		if not data:
-			break
-	data = data_to_send.decode(encoding='utf-8')
-	if data.startswith('+'):
-		return data
-	else:
-		print("connection lose")
-		sys.exit(0)
+def send_recv(command, sock):
+    command_to_send = bytes(command, 'utf-8') + b'\n'
+
+    sock.send(command_to_send)
+    data_to_send = b''
+    while True:
+        try:
+            data = sock.recv(1024)
+        except socket.timeout:
+            break
+        data_to_send += data
+        if not data:
+            break
+    data = data_to_send.decode(encoding='utf-8')
+    if data.startswith('+'):
+        return data
+    else:
+        print(data)
+        print("DROP SESSION, PLS WORK WITHOUT MISSTAKES")
+        sys.exit(0)
 
 
 LOGIN = 'nabgosha@yandex.ru'
@@ -35,110 +37,117 @@ ADDRESS = ('pop.yandex.ru', 995)
 
 
 def get_headers(input):
-	print(func_for_parse(r'(From: )=\?utf-8\?B\?(.*?)\?=', input, 'From: '))
-	print(re.findall(r'To: .*?\n', input)[0])
-	print(re.findall(r'Date: .*?\n', input)[0])
-	print(func_for_parse('(Subject: |\t)=\?utf-8\?B\?(.*?)\?=', input, 'Subject: '))
+    print(func_for_parse(r'(From: )=\?utf-8\?B\?(.*?)\?=', input, 'From: '))
+    print(re.findall(r'To: .*?\n', input)[0])
+    print(re.findall(r'Date: .*?\n', input)[0])
+    print(func_for_parse('(Subject: |\t)=\?utf-8\?B\?(.*?)\?=', input, 'Subject: '))
 
 
 def write(masbytes, filename):
-	result = base64.b64decode(masbytes)
-	with open(filename, 'wb') as file:
-		file.write(result)
+    result = base64.b64decode(masbytes)
+    with open(filename, 'wb') as file:
+        file.write(result)
 
 
 def get_full_message(message):
-	boundary = re.search('boundary="(.*?)"',message).group(1).replace('.','\.')
-	blocks = re.split('--'+boundary, message)[1:-1]
-	for block in blocks:
-		headers, masbytes = block.split('\r\n\r\n')
-		if '\r\nContent-Disposition: attachment;' in headers:
-			print(headers)
-			filename = re.findall('filename="(.*)"', headers)[0]
-			write(bytes(masbytes, 'utf-8'), filename)
-			print('file download, you can open them')
-		else:
-			print(headers)
-			text_message = base64.b64decode(masbytes).decode()
-			print(text_message)
+    boundary = re.search('boundary="(.*?)"', message)
+    if boundary:
+        boundary = boundary.group(1).replace('.', '\.')
+        blocks = re.split('--' + boundary, message)[1:-1]
+        split_by = '\r\n\r\n'
+    else:
+        blocks = [message]
+        split_by = '\r\n\r\n'
+    for block in blocks:
+        headers, masbytes = block.split(split_by)
+        if '\r\nContent-Disposition: attachment;' in headers:
+            print(headers)
+            filename = re.findall('filename="(.*)"', headers)[0]
+            write(bytes(masbytes, 'utf-8'), filename)
+            print('file download, you can open them')
+        else:
+            if 'Content-Transfer-Encoding: base64' in headers:
+                print(headers)
+                text_message = base64.b64decode(masbytes).decode()
+            else:
+                text_message = masbytes
+            if text_message[0] == '.':
+                text_message = text_message[1:]
+            text_message = text_message.replace('\n..', '\n.')
+            print(text_message.rstrip()[:-1])
 
 
 def func_for_parse(regex, input_str, header):
-	if re.findall(header + '=\?utf-8\?B', input_str) or re.findall(header + '=\?UTF-8\?B', input_str):
-		input_str=input_str.replace('UTF-8','utf-8')
-		string = [x[1] for x in re.findall(regex, input_str)]
-		string = [bytes(x, 'utf-8') for x in string]
-		return header + ''.join([base64.b64decode(x).decode('utf-8') for x in string])
-	else:
-		return re.findall(header + '.*\n', input_str)[0]
+    if re.findall(header + '=\?utf-8\?B', input_str, re.IGNORECASE):
+        string = [x[1] for x in re.findall(regex, input_str, re.IGNORECASE)]
+        string = [bytes(x, 'utf-8') for x in string]
+        return header + ''.join([base64.b64decode(x).decode('utf-8') for x in string])
+    else:
+        return re.findall(header + '.*\n', input_str, re.IGNORECASE)[0]
 
 
 def main():
-	global LOGIN, PASSWORD, ADDRESS
-	parser = ConfigParser()
-	with open('config.cfg', 'r', encoding='utf-8') as f:
-		parser.readfp(f)
-	LOGIN = parser['Account_from']['Login']
-	PASSWORD = parser['Account_from']['Password']
-	ADDRESS = (parser['Account_from']['Mail'], int(parser['Account_from']['Port']))
-	# try:
-	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-		sock = ssl.wrap_socket(sock)
-		sock.settimeout(1)
-		try:
-			sock.connect(ADDRESS)
-			print(sock.recv(1024).decode(encoding='utf-8'))
-			print(send_recv("USER " + LOGIN, sock))
-			print(send_recv("PASS " + PASSWORD, sock))
-		except socket.error:
-			print('cant connect')
-			sys.exit(0)
-		except Exception:
-			print('fail on try connect')
-			sys.exit(0)
+    global LOGIN, PASSWORD, ADDRESS
+    parser = ConfigParser()
+    with open('config.cfg', 'r', encoding='utf-8') as f:
+        parser.readfp(f)
+    LOGIN = parser['Account_from']['Login']
+    PASSWORD = parser['Account_from']['Password']
+    ADDRESS = (parser['Account_from']['Mail'], int(parser['Account_from']['Port']))
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock = ssl.wrap_socket(sock)
+        sock.settimeout(1)
+        try:
+            sock.connect(ADDRESS)
+            print(sock.recv(1024).decode(encoding='utf-8'))
+            print(send_recv("USER " + LOGIN, sock))
+            print(send_recv("PASS " + PASSWORD, sock))
+        except socket.error:
+            print('cant connect')
+            sys.exit(0)
+        except Exception:
+            print('fail on try connect')
+            sys.exit(0)
 
-		while True:
-			command = sys.stdin.readline().rstrip()
-			if command.startswith('RETR'):
-				print(BOUNDARY)
-				print(send_recv(command, sock))
-				print(BOUNDARY)
-				continue
-			if command.startswith('HEAD'):
-				print(BOUNDARY)
-				get_headers(send_recv('TOP ' + command.split()[1], sock))
-				print(BOUNDARY)
-				continue
-			if command.startswith('TOP'):
-				print(BOUNDARY)
-				print(send_recv(command, sock))
-				print(BOUNDARY)
-				continue
-			if command.startswith('FULL '):
-				print(BOUNDARY)
-				get_full_message(send_recv('RETR '+ command.split()[1], sock))
-				print(BOUNDARY)
-				continue
-			if command == 'STAT':
-				print(BOUNDARY)
-				print(send_recv("STAT", sock))
-				print(BOUNDARY)
-				continue
-			if command.startswith('LIST'):
-				print(BOUNDARY)
-				adds = '' if len(command.split()) < 2 else " " + command.split()[1]
-				print(send_recv("LIST" + adds, sock))
-				print(BOUNDARY)
-				continue
-			if command == 'QUIT':
-				print(send_recv("QUIT", sock))
-				break
-
-			print('unknown command')
-			print(BOUNDARY)
-	# except Exception:
-	#     print('cant connect')
+        while True:
+            command = sys.stdin.readline().rstrip()
+            if command.startswith('RETR'):
+                print(BOUNDARY)
+                print(send_recv(command, sock))
+                print(BOUNDARY)
+                continue
+            if command.startswith('HEAD'):
+                print(BOUNDARY)
+                get_headers(send_recv('TOP ' + command.split()[1], sock))
+                print(BOUNDARY)
+                continue
+            if command.startswith('TOP'):
+                print(BOUNDARY)
+                print(send_recv(command, sock))
+                print(BOUNDARY)
+                continue
+            if command.startswith('FULL '):
+                print(BOUNDARY)
+                get_full_message(send_recv('RETR ' + command.split()[1], sock))
+                print(BOUNDARY)
+                continue
+            if command == 'STAT':
+                print(BOUNDARY)
+                print(send_recv("STAT", sock))
+                print(BOUNDARY)
+                continue
+            if command.startswith('LIST'):
+                print(BOUNDARY)
+                adds = '' if len(command.split()) < 2 else " " + command.split()[1]
+                print(send_recv("LIST" + adds, sock))
+                print(BOUNDARY)
+                continue
+            if command == 'QUIT':
+                print(send_recv("QUIT", sock))
+                break
+            print('unknown command')
+            print(BOUNDARY)
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- tasks/pop3/test.py
+++ tasks/pop3/test.py
@@ -1,34 +1,90 @@
 import re
 import base64
-str = '''From: =?UTF-8?B?0JPQvtGI0LAg0J3QsNCx0L7QudGH0LXQvdC60L4=?= <gosha3548@gmail.com>
-Date: Mon, 7 May 2018 20:11:58 +0500
-Message-ID: <CAHN5DD5g4ZqBhrFYX5JHg3D1vgTEJatDE1cyM5BcCbApQOfudw@mail.gmail.com>
-Subject: =?UTF-8?B?dGVtYSBuYSBhbmdsINC4INC90LAg0YDRg9GB0YHQutC+0Lwg0L3QsCDRgNGD0YHRgdC6?=
-	=?UTF-8?B?0L7QvCDQvdCwINGA0YPRgdGB0LrQvtC8INC90LAg0YDRg9GB0YHQutC+0Lwg0L3QsCDRgNGD0YHRgdC6?=
-	=?UTF-8?B?0L7QvCDQvdCwINGA0YPRgdGB0LrQvtC8INC90LAg0YDRg9GB0YHQutC+0Lwg0L3QsCDRgNGD0YHRgdC6?=
-	=?UTF-8?B?0L7QvCDQvdCwINGA0YPRgdGB0LrQvtC8INGE0LjQvdCw0LvRjNC90LDRjyDRgdGC0YDQvtC60LA=?=
-To: nabgosha@yandex.ru
-Content-Type: multipart/mixed;
-	boundary="----==--bound.880449.web44g.yandex.ru"
-'''
+
+str = '''Received: from mxback5o.mail.yandex.net ([127.0.0.1])
+	by mxback5o.mail.yandex.net with LMTP id pjJXbb9D
+	for <nabgosha@yandex.ru>; Thu, 17 May 2018 13:31:28 +0300
+Received: from mxback5o.mail.yandex.net (localhost.localdomain [127.0.0.1])
+	by mxback5o.mail.yandex.net (Yandex) with ESMTP id C3C5E1E0116D
+	for <nabgosha@yandex.ru>; Thu, 17 May 2018 13:31:28 +0300 (MSK)
+Received: from localhost (localhost [::1])
+	by mxback5o.mail.yandex.net (nwsmtp/Yandex) with ESMTP id wCRIcWyxGp-VSN8NeIn;
+	Thu, 17 May 2018 13:31:28 +0300
+X-Yandex-Front: mxback5o.mail.yandex.net
+X-Yandex-TimeMark: 1526553088
+DKIM-Signature: v=1; a=rsa-sha256; c=relaxed/relaxed; d=yandex.ru; s=mail; t=1526553088;
+	bh=AgwToad/bWRgwGuggDg3NwaDEZvZmWAPhc5Hng8mUJA=;
+	h=From:To:Subject:Message-Id:Date;
+	b=dAvKdcHQrmhD0O8Nd5GOlccJGDzIQZCjiqGb2BGexq+7B1HsQ/6cRHzrLfXwUBj4D
+	 7Bkq1oiTK3Ev5yHesOYM3GxOCKfp3D4zj2xFuEIO4Ujo6fzxFs5/vL1wl8LAn141Q5
+	 igWCJDHaD8Jdb1glYfk/RMjjb6hLMoSiMX2oQtHA=
+Authentication-Results: mxback5o.mail.yandex.net; dkim=pass header.i=@yandex.ru
+X-Yandex-Spam: 1
+X-Yandex-Sender-Uid: 624227591
+Received: by web31o.yandex.ru with HTTP;
+	Thu, 17 May 2018 13:31:28 +0300
+From: =?utf-8?B?0LPQvtGI0LAg0L3QsNCx?= <nabgosha@yandex.ru>
+To: nabgosha <nabgosha@yandex.ru>
+Subject: anglish team
+MIME-Version: 1.0
+Message-Id: <12571241526553088@web31o.yandex.ru>
+X-Mailer: Yamail [ http://yandex.ru ] 5.0
+Date: Thu, 17 May 2018 15:31:28 +0500
+Content-Transfer-Encoding: 7bit
+Content-Type: text/plain
+Return-Path: nabgosha@yandex.ru
+X-YandexSms-Digest: b35b5f95f39e829b4bf4898bb0fd491c
+
+hello
+hello
+.
+..
+hello'''
+
+
+def get_full_message(message):
+	boundary = re.search('boundary="(.*?)"', message)
+	if boundary:
+		boundary = boundary.group(1).replace('.', '\.')
+		blocks = re.split('--' + boundary, message)[1:-1]
+		split_by = '\r\n\r\n'
+	else:
+		blocks = [message]
+		split_by = '\r\n'
+	for block in blocks:
+		headers, masbytes = block.split(split_by)
+		if '\r\nContent-Disposition: attachment;' in headers:
+			print(headers)
+			filename = re.findall('filename="(.*)"', headers)[0]
+			write(bytes(masbytes, 'utf-8'), filename)
+			print('file download, you can open them')
+		else:
+			if 'Content-Transfer-Encoding: base64' in headers:
+				print(headers)
+				text_message = base64.b64decode(masbytes).decode()
+			else:
+				text_message = masbytes
+			print(text_message)
+
+
 REGEX = re.compile('(Subject: |\t)=\?utf-8\?B\?(.*?)\?=')
+
+
 def get_headers(input):
-    print(func_for_parse(r'(From: )=\?utf-8\?B\?(.*?)\?=', input, 'From: '))
-    print(re.findall(r'To: .*?\n', input)[0])
-    print(re.findall(r'Date: .*?\n', input)[0])
-    print(func_for_parse('(Subject: |\t)=\?utf-8\?B\?(.*?)\?=', input, 'Subject: '))
+	print(func_for_parse(r'(From: )=\?utf-8\?B\?(.*?)\?=', input, 'From: '))
+	print(re.findall(r'To: .*?\n', input)[0])
+	print(re.findall(r'Date: .*?\n', input)[0])
+	print(func_for_parse('(Subject: |\t)=\?utf-8\?B\?(.*?)\?=', input, 'Subject: '))
 
 
 def func_for_parse(regex, input_str, header):
-    if re.findall(header + '=\?utf-8\?B', input_str) or re.findall(header + '=\?UTF-8\?B', input_str):
-        input_str=input_str.replace('UTF-8','utf-8')
-        string = [x[1] for x in re.findall(regex, input_str)]
-        string = [bytes(x, 'utf-8') for x in string]
-        return header + ''.join([base64.b64decode(x).decode('utf-8') for x in string])
-    else:
-        return re.findall(header + '.*\n', input_str)[0]
+	if re.findall(header + '=\?utf-8\?B', input_str) or re.findall(header + '=\?UTF-8\?B', input_str):
+		input_str = input_str.replace('UTF-8', 'utf-8')
+		string = [x[1] for x in re.findall(regex, input_str)]
+		string = [bytes(x, 'utf-8') for x in string]
+		return header + ''.join([base64.b64decode(x).decode('utf-8') for x in string])
+	else:
+		return re.findall(header + '.*\n', input_str)[0]
 
 
-boundary = re.search('boundary="(.*?)"',str).group(1).replace('.','\.')
-print(boundary)
-# get_headers(str)
+(get_full_message(str))
